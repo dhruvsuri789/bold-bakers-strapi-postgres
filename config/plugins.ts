9ea5b5299e3d48ce64fe317eb6a0bfb12640ae2b
Dhruv Suri <dhruvsuri789@gmail.com>
2025-02-06 06:39:03 +0530

    Added introspection in plugins.ts

--- config/plugins.ts
+++ config/plugins.ts
@@ -4,6 +4,7 @@ export default ({ env }) => ({
       endpoint: "/graphql",
       shadowCRUD: true,
       playgroundAlways: true, // <- Enable GraphQL Playground
+      introspection: true, // ✅ Enable GraphQL Introspection
       depthLimit: 10,
       amountLimit: 100,
     },
