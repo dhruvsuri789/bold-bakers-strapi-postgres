e81d535ac7fc9d5dc7106e6964a7ac58a988c8eb
Dhruv Suri <dhruvsuri789@gmail.com>
2025-02-06 06:52:04 +0530

    Added cors policy in middlleware.ts

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,12 +1,21 @@
 export default [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::logger",
+  "strapi::errors",
+  "strapi::security",
+  {
+    name: "strapi::cors",
+    config: {
+      enabled: true,
+      origin: ["*"], // Or specify Next.js URL: ["https://your-next-app.com"]
+      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+      headers: ["Content-Type", "Authorization"],
+      credentials: true,
+    },
+  },
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
